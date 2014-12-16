b1dc2b41f0ee047b1cba8e727b688fc9f9f65432
SergeyG <hatsanyuks@gmail.com>
2014-12-16 18:17:38 +0200

    Comment entity was added

---
+++ webroot/src/Acme/ProjBundle/Entity/Comment.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Acme\ProjBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ */
+class Comment
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $autor;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var string
+     */
+    private $site;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * @var integer
+     */
+    private $rating;
+
+    /**
+     * @var integer
+     */
+    private $status;
+
+    /**
+     * @var integer
+     */
+    private $failure;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set autor
+     *
+     * @param string $autor
+     * @return Comment
+     */
+    public function setAutor($autor)
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    /**
+     * Get autor
+     *
+     * @return string 
+     */
+    public function getAutor()
+    {
+        return $this->autor;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Comment
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set site
+     *
+     * @param string $site
+     * @return Comment
+     */
+    public function setSite($site)
+    {
+        $this->site = $site;
+
+        return $this;
+    }
+
+    /**
+     * Get site
+     *
+     * @return string 
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     * @return Comment
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Comment
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set failure
+     *
+     * @param integer $failure
+     * @return Comment
+     */
+    public function setFailure($failure)
+    {
+        $this->failure = $failure;
+
+        return $this;
+    }
+
+    /**
+     * Get failure
+     *
+     * @return integer 
+     */
+    public function getFailure()
+    {
+        return $this->failure;
+    }
+}
